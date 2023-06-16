736f9580935f87fe7a49983d02e4cbdf6aa351c0
GraceGrant <126471125+GraceGrant@users.noreply.github.com>
2023-06-16 13:36:02 +0200

    Update Bar_deaths.R

--- Question 1/code/Bar_deaths.R
+++ Question 1/code/Bar_deaths.R
@@ -1,12 +1,12 @@
 plot_deaths <- function(covid_cleaned) {
 
-    df <- covid_cleaned %>% group_by(location) %>% mutate(total_deaths = total_deaths / 1000000) %>%
-        summarise(total_deaths = sum(total_deaths))
+    df <- covid_cleaned %>% group_by(location) %>% mutate(new_deaths = ifelse(is.na(new_deaths), 0, new_deaths)) %>%
+        summarise(new_deaths = sum(new_deaths))
 
     bar_deaths <- df %>% ggplot() +
-        geom_bar(aes(x = location, y = total_deaths), stat = "identity", fill = "turquoise") +
+        geom_bar(aes(x = location, y = new_deaths), stat = "identity", fill = "turquoise") +
         theme_bw() +
-        labs(title = "Total Deaths per Continent", x = "Continent", y = "Total Deaths in Millions")
+        labs(title = "Total Deaths per Continent", x = "Continent", y = "Total Deaths")
 
     bar_deaths
 
